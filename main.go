6ad180f1527099e44a2b1f8f9bd55b73fd3c1fea
wangjinbei <wangjinbei@5eplay.com>
2025-03-21 11:40:27 +0800

    change file dir

--- main.go
+++ main.go
@@ -1,13 +1,7 @@
 package main
 
 import (
-	"context"
-	"fmt"
-	"log"
 	"net/http"
-	"os"
-	"os/signal"
-	"syscall"
 	"time"
 
 	"gowins/conf"
@@ -29,7 +23,6 @@ type ServerConfig struct {
 func main() {
 	// Load configuration
 	conf.LoadConfig()
-
 	cfg := &ServerConfig{}
 	if err := viper.Sub("server").Unmarshal(cfg); err != nil {
 		panic("Failed to parse config: " + err.Error())
@@ -44,8 +37,10 @@ func main() {
 	// Setup routes
 	routes.SetupRoutes(r)
 
+	// Setup loggers
 	logger.SetupAccLogger(r)
 
+	// Register middleware
 	// Set max body size middleware
 	if cfg.MaxBodySize > 0 {
 		r.Use(middlewares.SetupMaxBodySizeMiddleware(cfg.MaxBodySize))
@@ -58,32 +53,6 @@ func main() {
 		WriteTimeout: 5 * time.Second,
 	}
 
-	// Initializing the server in a goroutine so that
-	// it won't block the graceful shutdown handling below
-	go func() {
-		if err := srv.ListenAndServe(); err != nil && err != http.ErrServerClosed {
-			log.Fatalf("listen: %s\n", err)
-		}
-	}()
+	RegisterGraceful(srv)
 
-	// Wait for interrupt signal to gracefully shut down the server with
-	// a timeout of 5 seconds.
-	quit := make(chan os.Signal, 1)
-	// kill (no param) default send syscall.SIGTERM
-	// kill -2 is syscall.SIGINT
-	// kill -9 is syscall.SIGKILL but can't be caught, so don't need to add it
-	signal.Notify(quit, syscall.SIGINT, syscall.SIGTERM)
-	<-quit
-	log.Println("Shutting down server...")
-
-	// The context is used to inform the server it has 5 seconds to finish
-	// the request it is currently handling
-	fmt.Println(cfg.DownTime)
-	ctx, cancel := context.WithTimeout(context.Background(), time.Duration(cfg.DownTime)*time.Second)
-	defer cancel()
-	if err := srv.Shutdown(ctx); err != nil {
-		log.Fatal("Server forced to shutdown: ", err)
-	}
-
-	log.Println("Server exiting")
 }
